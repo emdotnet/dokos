5155b48fe2de1709cd27b8f574c09bf6ea3c6479
Charles-Henri Decultot <chdecultot@dokos.io>
2023-03-24 13:50:19 +0100

    feat: Closing balance for period closing and reporting

--- erpnext/regional/france/report/balance_sheet/balance_sheet.py
+++ erpnext/regional/france/report/balance_sheet/balance_sheet.py
@@ -48,6 +48,8 @@ def execute(filters=None):
 	period_list.extend(additional_columns)
 	period_list = list(reversed(sorted(period_list, key=lambda x: x["from_date"])))
 
+	filters.period_start_date = period_list[0]["year_start_date"]
+
 	currency = filters.presentation_currency or frappe.get_cached_value(
 		"Company", filters.company, "default_currency"
 	)
@@ -119,7 +121,7 @@ def execute(filters=None):
 	chart = get_chart_data(filters, columns, asset, liability, equity)
 
 	report_summary = get_report_summary(
-		period_list, asset, liability, equity, provisional_profit_loss, total_credit, currency, filters
+		period_list, asset, liability, equity, provisional_profit_loss, currency, filters
 	)
 
 	return columns, data, message, chart, report_summary
@@ -425,7 +427,6 @@ def get_report_summary(
 	liability,
 	equity,
 	provisional_profit_loss,
-	total_credit,
 	currency,
 	filters,
 	consolidated=False,
