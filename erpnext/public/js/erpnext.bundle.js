0782f599477653b1fb73672e13771fcb4c7989eb
Charles-Henri Decultot <chdecultot@dokos.io>
2023-05-30 08:47:49 +0200

    refactor!: Drop custom item quick entry

--- erpnext/public/js/erpnext.bundle.js
+++ erpnext/public/js/erpnext.bundle.js
@@ -12,7 +12,6 @@ import "./templates/booking_credit_dashboard.html";
 import "./utils/item_selector";
 import "./help_links";
 import "./templates/item_quick_entry.html";
-import "./utils/item_quick_entry";
 import "./utils/contact_address_quick_entry";
 import "./utils/customer_quick_entry";
 import "./utils/supplier_quick_entry";
