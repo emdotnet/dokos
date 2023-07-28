82fe04bf5192361166de231f0a678cd9f8bf8fdc
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2023-07-28 17:51:44 +0200

    feat(portal): Add button to disable address from user portal

--- erpnext/utilities/web_form/addresses/addresses.js
+++ erpnext/utilities/web_form/addresses/addresses.js
@@ -1,3 +1,9 @@
-frappe.ready(function() {
-	// bind events here
-})
+async function archive_this_address(name) {
+	await frappe.xcall("frappe.client.set_value", {
+		doctype: "Address",
+		name: name,
+		fieldname: "disabled",
+		value: 1,
+	});
+	window.location.href = "/{{ route }}/list";
+}
--- erpnext/utilities/web_form/addresses/addresses.py
+++ erpnext/utilities/web_form/addresses/addresses.py
@@ -4,3 +4,6 @@ import frappe
 def get_context(context):
 	# do your magic here
 	context.show_sidebar = True
+
+	if not frappe.form_dict.is_list:
+		context.template = "erpnext/utilities/web_form/addresses/addresses.html"
